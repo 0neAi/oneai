f78342ee92268c3022249e00500205e44a6c8080
0neAi <sayeemxgaming@gmail.com>
2025-05-02 03:43:22 +0000

    Update server.js

--- server.js
+++ server.js
@@ -19,8 +19,6 @@ app.use(helmet());
 app.use(cors({
   origin: [
     'https://0neai.github.io',
-    'http://localhost:5500',
-    'http://127.0.0.1:5500',
     'http://localhost:3000'
   ],
   methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
