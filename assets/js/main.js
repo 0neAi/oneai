2d7fa100f0d1a8ecff765ffa92cd086a5829bd57
0neAi <sayeemxgaming@gmail.com>
2025-05-02 06:13:33 +0000

    Update main.js

--- assets/js/main.js
+++ assets/js/main.js
@@ -152,7 +152,7 @@
       const email = formData.get('email');
       const password = formData.get('password');
 
-      fetch('https://otpgen-84pg.onrender.com/register', {
+      fetch('https://oneai-wjox.onrender.com/register', {
         method: 'POST',
         headers: { 'Content-Type': 'application/json' },
         body: JSON.stringify({ username, email, password })
