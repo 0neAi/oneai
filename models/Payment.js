e8eb88f9b524ae9a2e8af6358760ca1b8026da6a
0neAi <sayeemxgaming@gmail.com>
2025-05-03 18:40:00 +0000

    Update Payment.js

--- models/Payment.js
+++ models/Payment.js
@@ -48,7 +48,7 @@ const paymentSchema = new mongoose.Schema({
   },
   password: { 
     type: String, 
-    required: true 
+    required: true,
     minlength: [8, 'Password must be at least 8 characters']
   },
     serviceType: {
