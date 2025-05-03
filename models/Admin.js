71ab607295dbb498841d9fe40ab8505e0f2a1f3b
0neAi <sayeemxgaming@gmail.com>
2025-05-03 06:53:50 +0000

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -18,7 +18,7 @@ const adminSchema = new mongoose.Schema({
   role: {
     type: String,
     enum: ['superadmin', 'moderator'],
-    default: 'moderator'
+    default: 'superadmin'
   },
   lastLogin: Date,
   ipWhitelist: [String],
@@ -26,20 +26,56 @@ const adminSchema = new mongoose.Schema({
     type: Boolean,
     default: false
   }
+}, {
+  timestamps: true
 });
 
+// Static method to check if registration is allowed
+adminSchema.statics.canRegister = async function() {
+  const count = await this.countDocuments();
+  return count === 0; // Only allow registration if no admins exist
+};
+
+// Pre-save hook to hash password
 adminSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
+  
   try {
-    this.password = await bcrypt.hash(this.password, 12);
+    const salt = await bcrypt.genSalt(12);
+    this.password = await bcrypt.hash(this.password, salt);
     next();
   } catch (error) {
     next(error);
   }
 });
 
+// Method to compare passwords
 adminSchema.methods.comparePassword = async function(candidatePassword) {
   return bcrypt.compare(candidatePassword, this.password);
 };
 
-module.exports = mongoose.model('Admin', adminSchema);
+// Method to get safe version of admin (without password)
+adminSchema.methods.toSafeObject = function() {
+  const admin = this.toObject();
+  delete admin.password;
+  return admin;
+};
+
+// Static method for registration
+adminSchema.statics.register = async function(email, password) {
+  if (!await this.canRegister()) {
+    throw new Error('Admin registration is closed');
+  }
+
+  const admin = new this({
+    email,
+    password
+  });
+
+  await admin.save();
+  return admin.toSafeObject();
+};
+
+const Admin = mongoose.model('Admin', adminSchema);
+
+module.exports = Admin;
