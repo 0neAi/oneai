96f24dcab68c865aeb8cae91abfef8b25053c68e
0neAi <sayeemxgaming@gmail.com>
2025-05-04 20:53:25 +0000

    Update Admin.js

--- models/Admin.js
+++ models/Admin.js
@@ -35,11 +35,8 @@ const adminSchema = new mongoose.Schema({
 // Pre-save hook to hash password
 adminSchema.pre('save', async function(next) {
   if (!this.isModified('password')) return next();
-  this.password = await bcrypt.hash(this.password, 12);
-  next();
   try {
-    const salt = await bcrypt.genSalt(12);
-    this.password = await bcrypt.hash(this.password, salt);
+    this.password = await bcrypt.hash(this.password, 12); // ✅ Correct one-step hash
     next();
   } catch (error) {
     next(error);
