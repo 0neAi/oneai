d56c9a9e706334308dc18372884384f82ebe5c9b
0neAi <sayeemxgaming@gmail.com>
2025-07-06 06:54:47 -0700

    Update hackeranimation.js

--- assets/js/hackeranimation.js
+++ assets/js/hackeranimation.js
@@ -132,6 +132,30 @@
                 "> Verifying system integrity...",
                 "> Connection established",
                 "> System ready"
+                ">>CONNECTED"
+                ">>CONNECTED"
+                ">>CONNECTED"
+                "Refreshing Connection"
+                ">"
+                ">>"
+                ">>>"
+                ">>>>>>"
+                "Location:>"
+                "Location:>>>"
+                "Location:>>>>>:"
+                "Location:Success"
+                "Location:Bangladesh"
+                "Processing"
+                "Processing.."
+                "Processing...."
+                "Processing....>"
+                "Getting Data"
+                "Getting Data."
+                "Getting Data..."
+                "Getting Data....!"
+                "Connection established",
+                "Success"
+            
             ];
             
             let currentIndex = 0;
