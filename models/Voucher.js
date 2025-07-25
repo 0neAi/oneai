03bde939abbc0806fda8e4ffd87eca793d7a7bd9
0neAi <sayeemxgaming@gmail.com>
2025-07-25 11:58:06 -0700

    Add files via upload

---
+++ models/Voucher.js
@@ -0,0 +1,22 @@
+import mongoose from 'mongoose';
+
+const voucherSchema = new mongoose.Schema({
+    phone: { type: String, required: true },
+    code: { type: String, required: true, unique: true },
+    discountPercentage: { type: Number, required: true },
+    isUsed: { type: Boolean, default: false },
+    validUntil: { type: Date, required: false },
+    report: {
+        type: mongoose.Schema.Types.ObjectId,
+        refPath: 'reportModel'
+    },
+    reportModel: {
+        type: String,
+        required: true,
+        enum: ['MerchantIssue', 'PenaltyReport', 'PremiumService', 'User']
+    }
+}, { timestamps: true });
+
+const Voucher = mongoose.model('Voucher', voucherSchema);
+
+export default Voucher;
