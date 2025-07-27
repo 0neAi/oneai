b2c525d411a3cba0bc1b5925c5087175ddd958e4
0neAi <sayeemxgaming@gmail.com>
2025-07-27 00:39:27 -0700

    Update User.js

--- models/User.js
+++ models/User.js
@@ -2,12 +2,6 @@ import mongoose from 'mongoose';
 import bcrypt from 'bcrypt';
 
 const userSchema = new mongoose.Schema({
-  userId: {
-    type: String,
-    required: true,
-    unique: true,
-    default: () => Math.floor(10000000 + Math.random() * 90000000).toString(),
-  },
   name: {
     type: String,
     required: [true, 'Name is required'],
