a886dc87e3b717a4fc719ef494c0a9860b03671b
0neAi <sayeemxgaming@gmail.com>
2025-09-05 01:09:12 +0600

    Update User.js

--- models/User.js
+++ models/User.js
@@ -1,5 +1,5 @@
-import mongoose from 'mongoose';
-import bcrypt from 'bcrypt';
+const mongoose = require('mongoose');
+const bcrypt = require('bcrypt');
 
 const userSchema = new mongoose.Schema({
   name: {
@@ -78,4 +78,4 @@ userSchema.methods.comparePassword = async function(candidatePassword) {
 };
 
 const User = mongoose.model('User', userSchema);
-export default User;
+module.exports = User;
